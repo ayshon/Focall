b644d78a6a5c32d3bc2f4b49b53bde1e0db2e3f7
Joseph Neil Fontanilla <josephfontanilla2@gmail.com>
2022-10-24 22:18:21 -0400

    add editorconfig and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,43 @@
-import React from 'react';
-import './App.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 type resultProps = {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+};
 
 function App() {
-    const [result, setResult] = useState<resultProps[]>([]);
+  const [result, setResult] = useState<resultProps[]>([]);
 
+  useEffect(() => {
+    fetch("https://mergesharpwebapi.azurewebsites.net/weatherforecast")
+      .then((response) => response.json())
+      .then((res) => setResult(res))
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        fetch('https://mergesharpwebapi.azurewebsites.net/weatherforecast')
-            .then(response => response.json())
-            .then(res => setResult(res))
-            .catch(err => console.log(err))
-    }, []);
-
-    return (
-        <div className="App">
-            <table>
-                {result.map((value) => {
-                    return (
-                        <tbody>
-                            <tr>
-                                <td>{value.date}</td>
-                                <td>{value.temperatureC}</td>
-                                <td>{value.temperatureF}</td>
-                                <td>{value.summary}</td>
-                            </tr>
-                        </tbody>
-                    );
-                })}
-            </table>
-            <p>Learn React</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <table>
+        {result.map((value) => {
+          return (
+            <tbody>
+              <tr>
+                <td>{value.date}</td>
+                <td>{value.temperatureC}</td>
+                <td>{value.temperatureF}</td>
+                <td>{value.summary}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+      <p>Learn React</p>
+    </div>
+  );
 }
 
 export default App;
