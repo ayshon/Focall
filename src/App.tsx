bcd51ed71dbbcf6c227ae8d1e80085e1d5edb86e
Sean <seanpatrickllera@yahoo.ca>
2022-11-06 18:35:38 -0500

    Updated button states to be in an array

--- src/App.tsx
+++ src/App.tsx
@@ -11,69 +11,55 @@ function App() {
         // TODO: get server state
         // TODO: update INITIAL_STATE to have values from server
 
-    
-    const [checked_one, setOne] = React.useState(INITIAL_STATE[0]);
-    const [checked_two, setTwo] = React.useState(INITIAL_STATE[1]);
-    const [checked_three, setThree] = React.useState(INITIAL_STATE[2]);
-    const [checked_four, setFour] = React.useState(INITIAL_STATE[3]);
-    const [checked_five, setFive] = React.useState(INITIAL_STATE[4]);
-
-    const handleChange = (num: number) => {
-        switch(num){
-            case 1:
-                setOne(!checked_one);
-                // TODO: write the new checked_one value to the backend
-                break
-            case 2:
-                setTwo(!checked_two);
-                // TODO: write the new checked_two value to the backend
-                break
-            case 3:
-                setThree(!checked_three);
-                // TODO: write the new checked_three value to the backend
-                break
-            case 4:
-                setFour(!checked_four);
-                // TODO: write the new checked_four value to the backend
-                break
-            case 5:
-                setFive(!checked_five);
-                // TODO: write the new checked_five value to the backend
-                break
+    const buttons = [
+        {
+            "box_num": 11,
+            "value": INITIAL_STATE[0]
+        },
+        {
+            "box_num": 22,
+            "value": INITIAL_STATE[1]
+        },
+        {
+            "box_num": 33,
+            "value": INITIAL_STATE[2]
+        },
+        {
+            "box_num": 44,
+            "value": INITIAL_STATE[3]
+        },
+        {
+            "box_num": 55,
+            "value": INITIAL_STATE[4]
         }
-    };
+    ]
+
+    const [check_bottons, setButtons] = React.useState(buttons);
+    
+    const changeHandler = (num: number) => {
+        const newState = check_bottons.map(obj => {
+            if (obj.box_num === num) {
+              return {...obj, value: !obj.value};
+            }
+      
+            return obj;
+          });
+
+        setButtons(newState);
+    }
 
     return (
         <div className="App">
             <p>Learn React</p>
+            <div>
+                {check_bottons.map((item, i) => (<Checkbox onChange={changeHandler} value={item.value} box_num={item.box_num}/>))}
+            </div>
+            <p>Checkbox 11 state: {check_bottons[0].value.toString()}</p>
+            <p>Checkbox 22 state: {check_bottons[1].value.toString()}</p>
+            <p>Checkbox 33 state: {check_bottons[2].value.toString()}</p>
+            <p>Checkbox 44 state: {check_bottons[3].value.toString()}</p>
+            <p>Checkbox 55 state: {check_bottons[4].value.toString()}</p>
 
-            <Checkbox
-                onChange = {handleChange}
-                value = {checked_one}
-                box_num = {1}
-            />
-            <Checkbox
-                onChange = {handleChange}
-                value = {checked_two}
-                box_num = {2}
-            />
-            <Checkbox
-                onChange = {handleChange}
-                value = {checked_three}
-                box_num = {3}
-            />
-            <Checkbox
-                onChange = {handleChange}
-                value = {checked_four}
-                box_num = {4}
-            />
-            <Checkbox
-                onChange = {handleChange}
-                value = {checked_five}
-                box_num = {5}
-            />
-
-            <p>Checkbox state: {checked_one.toString()}</p>
         </div>
     );
 }
