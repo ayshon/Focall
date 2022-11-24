2129780fd4e1c1e18a808f1e86e628fae88eb11b
Joseph Neil Fontanilla <josephfontanilla2@gmail.com>
2022-11-23 20:48:05 -0500

    add gojs diagram to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,87 @@
 import React from "react";
-import "./App.css";
-import { useState, useEffect } from "react";
-import Checkbox from "./components/CheckboxComponent";
+import * as go from "gojs";
+import { ReactDiagram } from "gojs-react";
 import Checkboxes from "./components/Checkboxes";
 
+import "./App.css";
+
+/**
+ * Diagram initialization method, which is passed to the ReactDiagram component.
+ * This method is responsible for making the diagram and initializing the model and any templates.
+ * The model's data should not be set here, as the ReactDiagram component handles that via the other props.
+ */
+function initDiagram() {
+  const $ = go.GraphObject.make;
+  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
+  const diagram = $(go.Diagram, {
+    "undoManager.isEnabled": true, // must be set to allow for model change listening
+    // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
+    "clickCreatingTool.archetypeNodeData": {
+      text: "new node",
+      color: "lightblue",
+    },
+    model: new go.GraphLinksModel({
+      linkKeyProperty: "key", // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+    }),
+  });
+
+  // define a simple Node template
+  diagram.nodeTemplate = $(
+    go.Node,
+    "Auto", // the Shape will go around the TextBlock
+    new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
+      go.Point.stringify
+    ),
+    $(
+      go.Shape,
+      "RoundedRectangle",
+      { name: "SHAPE", fill: "white", strokeWidth: 0 },
+      // Shape.fill is bound to Node.data.color
+      new go.Binding("fill", "color")
+    ),
+    $(
+      go.TextBlock,
+      { margin: 8, editable: true }, // some room around the text
+      new go.Binding("text").makeTwoWay()
+    )
+  );
+
+  return diagram;
+}
+
+/**
+ * This function handles any changes to the GoJS model.
+ * It is here that you would make any updates to your React state, which is dicussed below.
+ */
+function handleModelChange(changes: any) {
+  console.log("GoJS model changed!");
+}
+
 function App() {
-    return (
-        <Checkboxes />
-    );
+  return (
+    <div>
+      <Checkboxes />
+      <ReactDiagram
+        initDiagram={initDiagram}
+        divClassName="diagram-component"
+        nodeDataArray={[
+          { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
+          { key: 1, text: "Beta", color: "orange", loc: "150 0" },
+          { key: 2, text: "Gamma", color: "lightgreen", loc: "0 150" },
+          { key: 3, text: "Delta", color: "pink", loc: "150 150" },
+        ]}
+        linkDataArray={[
+          { key: -1, from: 0, to: 1 },
+          { key: -2, from: 0, to: 2 },
+          { key: -3, from: 1, to: 1 },
+          { key: -4, from: 2, to: 3 },
+          { key: -5, from: 3, to: 0 },
+        ]}
+        onModelChange={handleModelChange}
+      />
+      ...
+    </div>
+  );
 }
 
 export default App;
